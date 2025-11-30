5a85829074eb045beb5ca58aa4170b418a202dea
hola <alvbryan800@gmail.com>
2025-11-29 21:09:15 -0500

    Create ChallengeCard.js

---
+++ ChallengeCard.js
@@ -0,0 +1,30 @@
+// src/data/challenges.js
+
+const kpopChallenges = [
+  {
+    id: 1,
+    grupo: "BTS",
+    cancion: "Dynamite",
+    dificultad: "Fácil",
+    duracion: "3:19",
+    urlVideo: "https://www.youtube.com/watch?v=baekgH1KqV4"
+  },
+  {
+    id: 2,
+    grupo: "BLACKPINK",
+    cancion: "DDU-DU DDU-DU",
+    dificultad: "Medio",
+    duracion: "3:29",
+    urlVideo: "https://www.youtube.com/watch?v=IHNzOHiIRi4"
+  },
+  {
+    id: 3,
+    grupo: "Stray Kids",
+    cancion: "God's Menu",
+    dificultad: "Difícil",
+    duracion: "3:01",
+    urlVideo: "https://www.youtube.com/watch?v=uD4gEaV9m54"
+  }
+];
+
+export default kpopChallenges;
